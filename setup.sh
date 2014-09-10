016216773f2f3950dcb3aac882f83d104b87f3e1
Florian Leitner <florian.leitner@gmail.com>
2014-09-10 16:52:36 +0200

    added barchart symlink to distribution

--- setup.sh
+++ setup.sh
@@ -12,6 +12,7 @@ ln -s $dir/distributionrc ~/.distributionrc
 mkdir -p ~/bin
 ln -s $dir/bin/* ~/bin/
 ln -s $dir/distribution/distribution ~/bin/
+ln -s $dir/distribution/distribution ~/bin/barchart
 ln -s $dir/z/z.sh ~/bin/z
 mkdir -p ~/man/man.1
 ln -s $dir/z/z.1 ~/man/man.1/
