5145ec012ce40a4927c1713d73d9b6c3943a50dc
Florian Leitner <me@fnl.es>
2020-12-13 21:11:10 +0100

    powerlevel10k for zsh setup

--- setup.sh
+++ setup.sh
@@ -14,6 +14,7 @@ mkdir $dir/tmp
 
 # install oh-my-zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
 
 # configuration files
 echo "linking configuration files from $dir/* to ~/.\\1"
